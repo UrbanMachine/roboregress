dc5b043e8f3fbd42be7c8ea79e2d972d5ed21ce8
Alex Thiele <apocthiel@gmail.com>
2022-07-16 10:40:54 -0700

    Update template

--- .github/check_lint.sh
+++ .github/check_lint.sh
@@ -6,5 +6,6 @@ poetry run mypy --ignore-missing-imports roboregress/
 poetry run isort --check --diff roboregress/ tests/
 poetry run black --check roboregress/ tests/
 poetry run flake8 roboregress/ tests/
-poetry run safety check -i 39462 -i 40291
 poetry run bandit -r roboregress/
+poetry run vulture
+echo "Lint successful!"
