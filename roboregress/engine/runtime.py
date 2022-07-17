38edb9c1a2ab79d63f53a74f66a91ed03d4c46fb
Alex Thiele <apocthiel@gmail.com>
2022-07-16 21:48:25 -0700

    Add basic EngineRuntime scaffolding

--- roboregress/engine/runtime.py
+++ roboregress/engine/runtime.py
@@ -1,2 +1,28 @@
+from typing import Set
+
+from roboregress.engine import BaseSimObject
+
+
 class EngineRuntime:
     """An object that can run the simulation engine"""
+
+    def __init__(self) -> None:
+        self._timestamp: float = 0
+        self._sim_objects: Set[BaseSimObject]
+
+    @property
+    def timestamp(self) -> float:
+        """A read-only getter for the timestamp property"""
+        return self._timestamp
+
+    def register(self, sim_object: BaseSimObject) -> None:
+        """Register a new sim object with the runtime"""
+        self._sim_objects.add(sim_object)
+
+    def _step(self) -> None:
+        pass
+
+    def run_until(self, timestamp: float) -> None:
+        """Run the engine until it is at or past the specified timestamp"""
+        while self._timestamp < timestamp:
+            self._step()
