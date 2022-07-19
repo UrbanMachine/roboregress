c0ea7156d5344af6abb25e494ed9c44ea01ffb35
Alex Thiele <apocthiel@gmail.com>
2022-07-19 14:33:19 -0700

    Add context manager for robot work tracking

--- roboregress/robot/statistics.py
+++ roboregress/robot/statistics.py
@@ -1,18 +1,36 @@
-from typing import Optional
+import contextlib
+from typing import Generator, Optional, Set
+
+from roboregress.engine import SimulationRuntime
 
 
 class RobotStats:
     """A stat tracker for a single robot cell side"""
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, runtime: SimulationRuntime):
         self.name = name
         self.total_time_working: float = 0
         self.total_time_slacking: float = 0
 
+        self._runtime = runtime
         self._last_work_start: Optional[float] = None
         self._last_work_end: Optional[float] = None
 
-    def start_working(self, time: float) -> None:
+    @property
+    def total_time(self) -> float:
+        return self.total_time_slacking + self.total_time_working
+
+    @contextlib.contextmanager
+    def track_work_time(self) -> Generator[None, None, None]:
+        """A context manager for tracking utilization of a robot"""
+
+        self.start_working()
+        yield
+        self.stop_working()
+
+    def start_working(self) -> None:
+        time = self._runtime.timestamp
+
         if self._last_work_end is not None:
             slack_time = time - self._last_work_end
             assert slack_time >= 0, "It's okay to not take breaks"
@@ -20,7 +38,9 @@ class RobotStats:
 
         self._last_work_start = time
 
-    def stop_working(self, time: float) -> None:
+    def stop_working(self) -> None:
+        time = self._runtime.timestamp
+
         assert self._last_work_start is not None
         assert time > self._last_work_start
         self._last_work_end = time
--- tests/robot/test_statistics.py
+++ tests/robot/test_statistics.py
@@ -1,19 +1,24 @@
+from unittest.mock import Mock
+
 from roboregress.robot.statistics import RobotStats
 
 
 def test_robot_stats():
-    robo_stats = RobotStats("bingus")
-
+    runtime = Mock()
+    robo_stats = RobotStats("bingus", runtime=runtime)
     assert robo_stats.total_time_slacking == 0
     assert robo_stats.total_time_working == 0
 
-    robo_stats.start_working(0)
-    robo_stats.stop_working(5)
+    runtime.timestamp = 0
+    with robo_stats.track_work_time():
+        runtime.timestamp = 5
 
     assert robo_stats.total_time_working == 5
     assert robo_stats.total_time_slacking == 0
 
-    robo_stats.start_working(7)
-    robo_stats.stop_working(8)
+    runtime.timestamp = 7
+    with robo_stats.track_work_time():
+        runtime.timestamp = 8
     assert robo_stats.total_time_working == 6
     assert robo_stats.total_time_slacking == 2
+    assert robo_stats.total_time == 8
