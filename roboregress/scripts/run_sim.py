56febc717be1e0101c2b9ceb4fac1d0273757c89
Alex Thiele <apocthiel@gmail.com>
2022-07-19 13:50:02 -0700

    ADd time control argument

--- roboregress/scripts/run_sim.py
+++ roboregress/scripts/run_sim.py
@@ -8,11 +8,18 @@ def main() -> None:
     parser = ArgumentParser()
     parser.add_argument("-v", "--visualize", action="store_true", default=False)
     parser.add_argument("-c", "--config", type=Path, required=True)
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=int,
+        default=8 * 60 * 60,
+        help="How long to run the simulation for. By default it will run for 8 hours",
+    )
     args = parser.parse_args()
 
     runtime = runtime_from_file(args.config)
 
-    runtime.step_until(timestamp=8 * 60 * 60, visualization=args.visualize)
+    runtime.step_until(timestamp=args.time, visualization=args.visualize)
     print("Finished Simulation!")
 
 
